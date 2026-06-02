33098ef9ca9edb0163eeb262c33402c3dfb7c1d8
Elliotlf001 <137006835+Elliotlf001@users.noreply.github.com>
2026-06-02 10:17:28 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import threading
+import requests
+from webview.webview import Webview
+
+
+def webview_start():
+    webview = Webview()
+    webview.title = "Wallet Pass Creator"
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    html_path = os.path.join(current_dir, 'website\\index.html')
+    webview.navigate(f"file://{html_path}")
+
+    # bind python to java script
+    webview.bind("fetch_title", fetch_title)
+    webview.bind("fetch_icon", fetch_icon)
+    webview.bind("fetch_website", fetch_description)
+
+    webview.run()
+
+def fetch_title(URL):
+    html = fetch_html_file(URL)
+    start = html.find("<title>")
+    end = html.find("</title>")
+    title = html[start+7:end]
+    print(title)
+    return title
+
+def fetch_icon(URL):
+    html = fetch_html_file(URL)
+    start = html.find('<link')
+    html_short = html[start:]
+    end = html_short.find(">")
+    icon = html_short[:end]
+    print(icon)
+    return icon
+
+def fetch_description(URL):
+    html = fetch_html_file(URL)
+    start = html.find("<description>")
+    end = html.find("</description>")
+    description = html[start+7:end]
+    return description
+    
+def fetch_html_file(URL):
+    response = requests.get(URL)
+    html = response.text
+    return html
+
+internal_browser = threading.Thread(target=webview_start)
+internal_browser.start()
---
+++ start.pyw
@@ -0,0 +1,7 @@
+import os
+
+try:
+    import main
+except:
+    os.system("pip install webview_python")
+    os.system("pip install requests")
