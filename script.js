24f03f79da0faa019a605e2eff415eab49658245
Seyar <seyar.omari1382@gmail.com>
2026-06-02 11:08:18 +0100

    Add color picker and confirm button to pass preview; update styles for layout

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ button.addEventListener("click", () => {
   user_url = document.getElementById("url_input").value;
   console.log(user_url);
   generatePass(user_url);
+  // fetchMetadata(user_url);
 });
 
 const passPreview = document.querySelector(".pass-preview");
@@ -17,10 +18,33 @@ const passPreview = document.querySelector(".pass-preview");
 function generatePass(url) {
   const textElement = document.createElement("p");
   textElement.textContent = "Creating pass for: " + url;
-  passPreview.appendChild(textElement);
+  passPreview.prepend(textElement);
   textElement.classList.add("pass-content");
+
+  const confirmButton = document.querySelector(".download");
+  confirmButton.url = "https://romax.co.uk"; // Store the URL in a custom property of the button
+  confirmButton.addEventListener("click", () => {
+    open(confirmButton.url, "_blank"); // Open the URL in a new tab
+    // alert("Pass created for: " + url);
+  });
 }
 
+// async function fetchMetadata(url) {
+//   const inputUrl = document.getElementById("url_input").value;
+
+//   const response = await fetch(
+//     `https://api.microlink.io/?url=${encodeURIComponent(url)}`,
+//   );
+
+//   const data = await response.json();
+
+//   document.getElementById("logo").src = data.data.logo?.url || "";
+
+//   document.getElementById("title").textContent = data.data.title || "Untitled";
+
+//   document.getElementById("description").textContent =
+//     data.data.description || "";
+// }
 const passContent = document.querySelector(".pass-js");
 
 const colorPicker = document.querySelector(".color-picker");
@@ -36,3 +60,9 @@ passContent.appendChild(logo);
 
 // const passContent = document.querySelector(".pass-js");
 // passContent.classList.add("pass-content");
+async function fetchMetadata(url) {
+  fetch_title(url);
+  fetch_icon(url);
+  fetch_description(url);
+  fetch_html_file(url);
+}
